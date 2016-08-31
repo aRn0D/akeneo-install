cd33b08a19ba5684c2f05ef717cba8966d066970
aRn0D <arnaud.langlade@akeneo.com>
2016-08-31 21:43:28 +0200

    Wrong script call

--- pim/install-all-pim.sh
+++ pim/install-all-pim.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-# Example : install-all-pim.sh /path/to/ odm
+# Example : install-all-pim.sh /path/to/
 
-ROOT_INSTALLER=$(pwd)
+REAL_SCRIPT=$(readlink -f $0)
+ROOT_INSTALLER=$(dirname $REAL_SCRIPT)
 
 sudo php5dismod xdebug; /etc/init.d/apache2 restart
 
-$ROOT_INSTALLER/install-pim.sh "pee14" "1.4" "$1" "$2"
-$ROOT_INSTALLER/install-pim.sh "peem14" "1.4" "$1" "$2" "odm"
-$ROOT_INSTALLER/install-pim.sh "pee15" "1.5" "$1" "$2"
-$ROOT_INSTALLER/install-pim.sh "peem15" "1.5" "$1" "$2" "odm"
-$ROOT_INSTALLER/install-pim.sh "pee16" "1.6" "$1" "$2"
-$ROOT_INSTALLER/install-pim.sh "peem16" "1.6" "$1" "$2" "odm"
-$ROOT_INSTALLER/install-pim.sh "pee" "master" "$1" "$2"
-$ROOT_INSTALLER/install-pim.sh "peem" "master" "$1" "$2" "odm"
+$ROOT_INSTALLER/create-project "pee14" "1.4" "$1"
+$ROOT_INSTALLER/create-project.sh "peem14" "1.4" "$1" "odm"
+$ROOT_INSTALLER/create-project.sh "pee15" "1.5" "$1"
+$ROOT_INSTALLER/create-project.sh "peem15" "1.5" "$1" "odm"
+$ROOT_INSTALLER/create-project.sh "pee16" "1.6" "$1"
+$ROOT_INSTALLER/create-project.sh "peem16" "1.6" "$1" "odm"
+$ROOT_INSTALLER/create-project.sh "pee" "master" "$1"
+$ROOT_INSTALLER/create-project.sh "peem" "master" "$1" "odm"
